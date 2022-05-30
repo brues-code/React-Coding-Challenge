6c044ff25c019c80e4ca6acbf8322b0658a2cc76
Skyler H <skylerh15@github.com>
2022-05-30 11:19:34 -0500

    store ventures in context

--- src/app/context/AppContext.tsx
+++ src/app/context/AppContext.tsx
@@ -1,17 +1,25 @@
 import React, { createContext, FC, useContext, useMemo } from 'react';
 
-interface State {}
+import { Venture } from 'types/venture';
+
+import funds from 'mocks/funds.json';
+
+interface State {
+    ventures: Venture[];
+}
 
 interface ApiProps {}
 
 type AppState = State & ApiProps;
 
-const initialState: AppState = {};
+const initialState: AppState = {
+    ventures: []
+};
 
 export const AppContext = createContext(initialState);
 
 const AppContextProvider: FC = ({ children }) => {
-    const contextState: AppState = useMemo(() => ({}), []);
+    const contextState: AppState = useMemo(() => ({ ventures: funds }), []);
 
     return <AppContext.Provider value={contextState}>{children}</AppContext.Provider>;
 };
