d3999373d086946b216fa087a3e0141761a0e731
Skyler H <skylerh15@github.com>
2022-05-30 10:39:07 -0500

    added typings

---
+++ src/types/company.ts
@@ -0,0 +1,9 @@
+export interface Company {
+    id: number;
+    name: string;
+    logo: string;
+    cost: number;
+    ownershipPercentage: number;
+    impliedValue: number;
+    founded: string;
+}
