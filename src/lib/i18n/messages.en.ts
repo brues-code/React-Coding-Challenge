a08e6e4059da5715b656de8352cdad286c370671
Skyler H <skylerh15@github.com>
2022-05-31 13:43:47 -0500

    adding 404 i18n message

--- src/lib/i18n/messages.en.ts
+++ src/lib/i18n/messages.en.ts
@@ -1,6 +1,7 @@
 import { Messages } from 'types/i18n';
 
 const messages: Messages = {
+    '404.error.message': 'Sorry, the resource you were looking for could not be found.',
     'aumni.logo.alt': 'Aumni Fund',
     'default.document.title': 'Aumni Fund',
     index: 'Index',
