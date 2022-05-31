97b88a6166680455ed8d58e49cd857cfffaba9ea
Skyler H <skylerh15@github.com>
2022-05-31 16:33:21 -0500

    Add Avatar support to ListItemLink

--- src/app/components/ListItemLink/ListItemLink.tsx
+++ src/app/components/ListItemLink/ListItemLink.tsx
@@ -1,14 +1,20 @@
 import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
-import { ListItem, ListItemText, LinkProps } from '@material-ui/core';
+import { ListItem, ListItemText, LinkProps, ListItemAvatar, Avatar, AvatarProps } from '@material-ui/core';
 
 interface OwnProps extends Omit<LinkProps, 'ref'> {
     selected?: boolean;
     to: string;
+    avatar?: AvatarProps;
 }
 
-const ListItemLink: FC<OwnProps> = ({ to, children, ...rest }) => (
+const ListItemLink: FC<OwnProps> = ({ to, children, avatar, ...rest }) => (
     <ListItem button component={Link} to={to} {...rest}>
+        {avatar && (
+            <ListItemAvatar>
+                <Avatar {...avatar} />
+            </ListItemAvatar>
+        )}
         <ListItemText>{children}</ListItemText>
     </ListItem>
 );
