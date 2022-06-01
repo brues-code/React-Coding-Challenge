f7e3ac39386c9d9c6f304d5dffbd6c8e2dc4cfa1
Skyler H <skylerh15@github.com>
2022-05-31 20:44:28 -0500

    utilize RouteLink for logo home redirect

--- src/app/components/Topbar/Topbar.tsx
+++ src/app/components/Topbar/Topbar.tsx
@@ -4,21 +4,22 @@ import { useIntl } from 'react-intl';
 import { HOME } from 'app/route-paths';
 
 import FundSelector from 'app/components/FundSelector';
+import RouteLink from 'app/components/RouteLink';
 
 import logo from 'images/logo.png';
-import { TopbarContainer, Header, AumniLogo, LogoLink, FundSelectorContainer } from './styles';
+import { TopbarContainer, Header, AumniLogo, LogoContainer } from './styles';
 
 const Topbar: React.FC = () => {
     const { formatMessage } = useIntl();
     return (
         <TopbarContainer>
             <Header>
-                <LogoLink to={HOME}>
-                    <AumniLogo src={logo} alt={formatMessage({ id: 'aumni.logo.alt' })} />
-                </LogoLink>
-                <FundSelectorContainer>
-                    <FundSelector />
-                </FundSelectorContainer>
+                <LogoContainer>
+                    <RouteLink to={HOME}>
+                        <AumniLogo src={logo} alt={formatMessage({ id: 'aumni.logo.alt' })} />
+                    </RouteLink>
+                </LogoContainer>
+                <FundSelector />
             </Header>
         </TopbarContainer>
     );
