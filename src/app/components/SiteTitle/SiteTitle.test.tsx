cbd46f3e9eeca6c86f898cb0c690ed6443b93774
Skyler H <skylerh15@github.com>
2022-06-02 07:41:38 -0500

    test for documentTitle change

--- src/app/components/SiteTitle/SiteTitle.test.tsx
+++ src/app/components/SiteTitle/SiteTitle.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
 import Providers from 'app/context/Providers';
@@ -7,13 +7,18 @@ import Providers from 'app/context/Providers';
 import SiteTitle from './SiteTitle';
 
 describe('<SiteTitle />', () => {
+    const SiteTitleTest = () => (
+        <MemoryRouter>
+            <Providers>
+                <SiteTitle documentTitle={'TEST'} />
+            </Providers>
+        </MemoryRouter>
+    );
     it('renders', () => {
-        render(
-            <MemoryRouter>
-                <Providers>
-                    <SiteTitle documentTitle={'TEST'} />
-                </Providers>
-            </MemoryRouter>
-        );
+        render(<SiteTitleTest />);
+    });
+    it('changes documentTitle', async () => {
+        render(<SiteTitleTest />);
+        await waitFor(() => expect(document.title).toEqual('TEST | Aumni Fund'));
     });
 });
